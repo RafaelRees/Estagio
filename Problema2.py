1e5b78744140722679b7800cf7fc6b8e7ab4addf
Rafael Rees Gonçalves <78436341+RafaelRees@users.noreply.github.com>
2021-02-21 17:35:46 -0300

    Update Problema2.py

--- Problema2.py
+++ Problema2.py
@@ -1,6 +1,7 @@
 def exibir_matriz(matriz):
     for linha in matriz:
         print(linha)
+
 diagonalEsquerda = 0
 diagonalDireita = 0
 
@@ -13,17 +14,15 @@ for i in range(L):
         elemento = input("Elemento da linha {} e coluna {}: ".format(i,j))
         linha.append(int(elemento))
     matriz.append(linha)
+
 for i in range(L):
-    for j in range(L):
-        if i == j:
-            diagonalEsquerda += matriz[i][j]
+    diagonalEsquerda += matriz[i][i]
+
 for i in range(L):
-    for j in range(L):
-        if i+j == L-1:
-            diagonalDireita += matriz[i][j]
-          
+    diagonalDireita += matriz[i][L-1 - i]
+
 exibir_matriz(matriz)
 
 print('Diagonal Esquerda: ', diagonalEsquerda) 
-print('Diagonal Direita: ',diagonalDireita)    
-print('Saída: ', abs(diagonalEsquerda - diagonalDireita)) 
+print('Diagonal Direita: ',diagonalDireita)
+print('Saída: ', abs(diagonalEsquerda - diagonalDireita))
